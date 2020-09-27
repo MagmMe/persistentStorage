dcc5c69601fe3ce4e8cec9c9374364710dea938a
MagmMe <marcin@magm.me>
2020-09-27 07:36:24 +0200

    Creating functions witch insert and fetch user data to datbase

--- Persistent starage/ViewController.swift
+++ Persistent starage/ViewController.swift
@@ -11,7 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        Ta funkcja wstawi nowy klucz do bazy danych
+        UserDefaults.standard.set("Maks", forKey: "imie")
+        
+//        Ta funkcja ja odczyta
+        let user = UserDefaults.standard.object(forKey: "imie") as! String
+        print(user)
+        
+        
+//        Poniżej przechowywanie i pobieranie tablic
+        let arr = [2,34,4]
+        UserDefaults.standard.set(arr, forKey: "array")
+        var calledArray = UserDefaults.standard.object(forKey: "array") as! NSArray
+        print(calledArray)
+        
+        
     }
 
 
--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 - przechowywanie trwale w lokalnej bazie danych, danych uytkownika na urzadzeniu. 
 
 1. Tworzenie `Userdefaults`
+
+2. Przypisujemy nową wartos do bazy danyc
+`UserDefaults.standard.set("Maks", forKey: "imie")`
+
+3. Fetch/pobieramy funkcje z bazy danych. 
