902d6434424981d3470f9c69522f6993bf22356e
MagmMe <marcin@magm.me>
2020-09-27 06:52:56 +0200

    Initial Commit

---
+++ Persistent starage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Persistent starage
+//
+//  Created by Marcin Magiera on 27/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
